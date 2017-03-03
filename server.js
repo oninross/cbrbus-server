9841840ef1d633ad76b690a0e6d375bb9c4fba7d
Nino Ross Rodriguez <ninoross.rodriguez@adelphi.digital>
2017-03-03 18:04:12 +1100

    - attempt to fix push notifications >_<

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 var $ = require('jquery'),
-    jsdom = require("jsdom"),
-    express = require('express'),
-    app = express(),
-    webPush = require('web-push'),
-    bodyParser = require('body-parser'),
-    vehicleRef,
-    refreshInterval;
+jsdom = require("jsdom"),
+express = require('express'),
+app = express(),
+webPush = require('web-push'),
+bodyParser = require('body-parser'),
+vehicleRef,
+refreshInterval;
 
 const vapidKeys = webPush.generateVAPIDKeys();
 const API_KEY = 'A6F762'; // Development
@@ -32,118 +32,146 @@ app.get('/getPublicKey', function (req, res) {
 
 app.post('/register', function (req, res) {
     // A real world application would store the subscription info.
+    console.log('register::')
     res.sendStatus(201);
 });
 
 app.post('/sendNotification', function (req, res) {
-    clearInterval(refreshInterval);
-
-    const pushSubscriptions = {
-        endpoint: req.body.endpoint,
-        keys: {
-            p256dh: req.body.key,
-            auth: req.body.authSecret
-        }
-    },
-    payload = '',
-    options = {
-        vapidDetails: {
-            subject: 'mailto:cbrbusissues@gmail.com',
-            publicKey: vapidKeys.publicKey,
-            privateKey: vapidKeys.privateKey
-        }
-    };
-
-    webPush.setGCMAPIKey('135415812168');
-
-    var busId = req.body.busId,
-        busStopId = req.body.busStopId,
-        $xml = '';
-
-    vehicleRef = req.body.vehicleRef;
-
-    if (busId < 10) {
-        busId = '000' + busId.toString();
-    } else if (busId < 100) {
-        busId = '00' + busId.toString();
-    } else if (busId < 1000) {
-        busId = '0' + busId.toString();
-    }
-
-    $xml = '<?xml version="1.0" encoding="iso-8859-1" standalone="yes"?>';
-    $xml += '<Siri version="2.0" xmlns:ns2="http://www.ifopt.org.uk/acsb" xmlns="http://www.siri.org.uk/siri" xmlns:ns4="http://datex2.eu/schema/2_0RC1/2_0" xmlns:ns3="http://www.ifopt.org.uk/ifopt">';
-
-    $xml += '<ServiceRequest>';
-    $xml += '<RequestTimestamp>' + new Date().toISOString() + '</RequestTimestamp>';
-    $xml += '<RequestorRef>' + API_KEY + '</RequestorRef>';
-    $xml += '<VehicleMonitoringRequest version="2.0">';
-    $xml += '<RequestTimestamp>' + new Date().toISOString() + '</RequestTimestamp>';
-    $xml += '<VehicleMonitoringRef>VM_ACT_' + busId + '</VehicleMonitoringRef>';
-    $xml += '</VehicleMonitoringRequest>';
-    $xml += '</ServiceRequest>';
-
-    $xml += '</Siri>';
-
-    console.log('sendNotification::');
-    jsdom.env('', ['http://code.jquery.com/jquery.min.js'], function (err, window) {
-        var $ = window.$;
-        $.support.cors = true;
-
-        refreshInterval = setInterval(function () {
-            console.log('refresh::');
-            $.ajax({
-                url: 'https://cors-anywhere.herokuapp.com/http://siri.nxtbus.act.gov.au:11000/' + API_KEY + '/vm/service.xml',
-                data: $xml,
-                type: 'POST',
-                contentType: 'text/xml',
-                dataType: "text",
-                success: function (xml) {
-                    console.log('success::');
-                    var xmlDoc = $.parseXML(xml),
-                        $xml = $(xmlDoc),
-                        isNextStop = false;
-
-                    // console.log(xmlDoc);
-                    var $vehicleActivity = $xml.find('VehicleActivity'),
-                        $v,
-                        $vehicleLat,
-                        $vehicleLng,
-                        onwardCall,
-                        stopPointRef;
-
-                    $.each($vehicleActivity, function (i, v) {
-                        $v = $(v);
-                        onwardCall = $v.find('OnwardCall');
-                        stopPointRef = $(onwardCall).find('StopPointRef');
-
-                        if (stopPointRef[0] != undefined && vehicleRef[0] != undefined) {
-                            console.log(stopPointRef[0].innerHTML + ' == ' +  busStopId);
-                            if (Number(stopPointRef[0].innerHTML) == Number(busStopId)) {
-                                isNextStop = true;
-                                return false;
-                            }
-                        }
-                    });
-
-                    if (isNextStop) {
-                        clearInterval(refreshInterval);
-
-                        webPush.sendNotification(
-                            pushSubscriptions,
-                            payload,
-                            options
-                        );
-                    }
-                },
-                error: function (error) {
-                    console.log(error);
-                }
-            });
-        }, 10000);
+    setTimeout(function () {
+        webPush.sendNotification({
+            endpoint: req.body.endpoint,
+            TTL: 0,
+            keys: {
+                p256dh: req.body.key,
+                auth: req.body.authSecret
+            }
+        }, '')
+        .then(function () {
+            res.sendStatus(201);
+        })
+        .catch(function (error) {
+            console.log(error);
+            res.sendStatus(500);
+        });
     });
-
-    res.send('OK');
 });
+
+// app.post('/sendNotification', function (req, res) {
+//     clearInterval(refreshInterval);
+
+//     const pushSubscriptions = {
+//         endpoint: req.body.endpoint,
+//         keys: {
+//             p256dh: req.body.key,
+//             auth: req.body.authSecret
+//         }
+//     },
+//     payload = '',
+//     options = {
+//         vapidDetails: {
+//             subject: 'mailto:cbrbusissues@gmail.com',
+//             publicKey: vapidKeys.publicKey,
+//             privateKey: vapidKeys.privateKey
+//         }
+//     };
+
+//     webPush.setGCMAPIKey('135415812168');
+
+//     webPush.sendNotification(
+//         pushSubscriptions,
+//         payload,
+//         options
+//     );
+
+//     // var busId = req.body.busId,
+//     //     busStopId = req.body.busStopId,
+//     //     $xml = '';
+
+//     // vehicleRef = req.body.vehicleRef;
+
+//     // if (busId < 10) {
+//     //     busId = '000' + busId.toString();
+//     // } else if (busId < 100) {
+//     //     busId = '00' + busId.toString();
+//     // } else if (busId < 1000) {
+//     //     busId = '0' + busId.toString();
+//     // }
+
+//     // $xml = '<?xml version="1.0" encoding="iso-8859-1" standalone="yes"?>';
+//     // $xml += '<Siri version="2.0" xmlns:ns2="http://www.ifopt.org.uk/acsb" xmlns="http://www.siri.org.uk/siri" xmlns:ns4="http://datex2.eu/schema/2_0RC1/2_0" xmlns:ns3="http://www.ifopt.org.uk/ifopt">';
+
+//     // $xml += '<ServiceRequest>';
+//     // $xml += '<RequestTimestamp>' + new Date().toISOString() + '</RequestTimestamp>';
+//     // $xml += '<RequestorRef>' + API_KEY + '</RequestorRef>';
+//     // $xml += '<VehicleMonitoringRequest version="2.0">';
+//     // $xml += '<RequestTimestamp>' + new Date().toISOString() + '</RequestTimestamp>';
+//     // $xml += '<VehicleMonitoringRef>VM_ACT_' + busId + '</VehicleMonitoringRef>';
+//     // $xml += '</VehicleMonitoringRequest>';
+//     // $xml += '</ServiceRequest>';
+
+//     // $xml += '</Siri>';
+
+//     // console.log('sendNotification::');
+//     // jsdom.env('', ['http://code.jquery.com/jquery.min.js'], function (err, window) {
+//     //     var $ = window.$;
+//     //     $.support.cors = true;
+
+//     //     refreshInterval = setInterval(function () {
+//     //         console.log('refresh::');
+//     //         $.ajax({
+//     //             url: 'https://cors-anywhere.herokuapp.com/http://siri.nxtbus.act.gov.au:11000/' + API_KEY + '/vm/service.xml',
+//     //             data: $xml,
+//     //             type: 'POST',
+//     //             contentType: 'text/xml',
+//     //             dataType: "text",
+//     //             success: function (xml) {
+//     //                 console.log('success::');
+//     //                 var xmlDoc = $.parseXML(xml),
+//     //                     $xml = $(xmlDoc),
+//     //                     isNextStop = false;
+
+//     //                 // console.log(xmlDoc);
+//     //                 var $vehicleActivity = $xml.find('VehicleActivity'),
+//     //                     $v,
+//     //                     $vehicleLat,
+//     //                     $vehicleLng,
+//     //                     onwardCall,
+//     //                     stopPointRef;
+
+//     //                 $.each($vehicleActivity, function (i, v) {
+//     //                     $v = $(v);
+//     //                     onwardCall = $v.find('OnwardCall');
+//     //                     stopPointRef = $(onwardCall).find('StopPointRef');
+
+//     //                     if (stopPointRef[0] != undefined && vehicleRef[0] != undefined) {
+//     //                         console.log(stopPointRef[0].innerHTML + ' == ' +  busStopId);
+//     //                         if (Number(stopPointRef[0].innerHTML) == Number(busStopId)) {
+//     //                             isNextStop = true;
+//     //                             return false;
+//     //                         }
+//     //                     }
+//     //                 });
+
+//     //                 if (isNextStop) {
+//     //                     clearInterval(refreshInterval);
+
+//     //                     webPush.sendNotification(
+//     //                         pushSubscriptions,
+//     //                         payload,
+//     //                         options
+//     //                     );
+//     //                 }
+//     //             },
+//     //             error: function (error) {
+//     //                 console.log(error);
+//     //             }
+//     //         });
+//     //     }, 10000);
+//     // });
+
+//     res.send('OK');
+// });
+
 
 app.listen(process.env.PORT || 8888, function () {
     console.log('Example app listening on port 8888!');
