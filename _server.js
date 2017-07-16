6e82a555800605474c39a2e2a52e670b635dc506
Nino Ross Rodriguez <oninross@gmail.com>
2017-07-17 09:42:20 +1000

    - removed js-dom

--- _server.js
+++ _server.js
@@ -1,5 +1,4 @@
 var $ = require('jquery'),
-    jsdom = require("jsdom"),
     express = require('express'),
     app = express(),
     webPush = require('web-push'),
@@ -13,8 +12,8 @@ const vapidKeys = {
     privateKey: 'YQsK3rEMKv_RpectJAKZLpT1KxzQplVsLxSHxJ7dGP8'
 };
 
-const API_KEY = 'A6F762'; // Development
-// const API_KEY = 'AE9887'; // Production
+// const API_KEY = 'A6F762'; // Development
+const API_KEY = 'AE9887'; // Production
 
 app.use(express.static(__dirname + '/'));
 app.use(bodyParser.json());
@@ -73,71 +72,69 @@ app.post('/sendNotification', function (req, res) {
     }
 
     console.log('sendNotification::');
-    jsdom.env('', ['http://code.jquery.com/jquery.min.js'], function (err, window) {
-        var $ = window.$;
-        $.support.cors = true;
+    var $ = window.$;
+    $.support.cors = true;
 
-        refreshInterval = setInterval(function () {
-            console.log('refresh::');
+    refreshInterval = setInterval(function () {
+        console.log('refresh::');
 
-            $.ajax({
-                url: 'https://cors-anywhere.herokuapp.com/http://siri.nxtbus.act.gov.au:11000/' + API_KEY + '/vm/service.xml',
-                data: '<?xml version="1.0" encoding="iso-8859-1" standalone="yes"?><Siri version="2.0" xmlns:ns2="http://www.ifopt.org.uk/acsb" xmlns="http://www.siri.org.uk/siri" xmlns:ns4="http://datex2.eu/schema/2_0RC1/2_0" xmlns:ns3="http://www.ifopt.org.uk/ifopt"><ServiceRequest><RequestTimestamp>' + new Date().toISOString() + '</RequestTimestamp><RequestorRef>' + API_KEY + '</RequestorRef><VehicleMonitoringRequest version="2.0"><RequestTimestamp>' + new Date().toISOString() + '</RequestTimestamp><VehicleMonitoringRef>VM_ACT_' + busId + '</VehicleMonitoringRef></VehicleMonitoringRequest></ServiceRequest></Siri>',
-                type: 'POST',
-                contentType: 'text/xml',
-                dataType: "text",
-                success: function (xml) {
-                    console.log('success::');
+        $.ajax({
+            url: 'https://cors-anywhere.herokuapp.com/http://siri.nxtbus.act.gov.au:11000/' + API_KEY + '/vm/service.xml',
+            data: '<?xml version="1.0" encoding="iso-8859-1" standalone="yes"?><Siri version="2.0" xmlns:ns2="http://www.ifopt.org.uk/acsb" xmlns="http://www.siri.org.uk/siri" xmlns:ns4="http://datex2.eu/schema/2_0RC1/2_0" xmlns:ns3="http://www.ifopt.org.uk/ifopt"><ServiceRequest><RequestTimestamp>' + new Date().toISOString() + '</RequestTimestamp><RequestorRef>' + API_KEY + '</RequestorRef><VehicleMonitoringRequest version="2.0"><RequestTimestamp>' + new Date().toISOString() + '</RequestTimestamp><VehicleMonitoringRef>VM_ACT_' + busId + '</VehicleMonitoringRef></VehicleMonitoringRequest></ServiceRequest></Siri>',
+            type: 'POST',
+            contentType: 'text/xml',
+            dataType: "text",
+            success: function (xml) {
+                console.log('success::');
 
-                    var xmlDoc = $.parseXML(xml),
-                        $xml = $(xmlDoc),
-                        isNextStop = false;
+                var xmlDoc = $.parseXML(xml),
+                    $xml = $(xmlDoc),
+                    isNextStop = false;
 
-                    // console.log(xmlDoc);
-                    var $vehicleActivity = $xml.find('VehicleActivity'),
-                        $v,
-                        $vehicleLat,
-                        $vehicleLng,
-                        stopPointRef,
-                        vehicleRef,
-                        vehicleRefNum;
+                // console.log(xmlDoc);
+                var $vehicleActivity = $xml.find('VehicleActivity'),
+                    $v,
+                    $vehicleLat,
+                    $vehicleLng,
+                    stopPointRef,
+                    vehicleRef,
+                    vehicleRefNum;
 
-                    $.each($vehicleActivity, function (i, v) {
-                        $v = $(v);
-                        stopPointRef = $v.find('StopPointRef');
-                        vehicleRef = $v.find('VehicleRef');
+                $.each($vehicleActivity, function (i, v) {
+                    $v = $(v);
+                    stopPointRef = $v.find('StopPointRef');
+                    vehicleRef = $v.find('VehicleRef');
 
-                        if (stopPointRef[0] != undefined && vehicleRef[0] != undefined) {
-                            console.log(stopPointRef[0].innerHTML + ' == ' +  busStopId);
+                    if (stopPointRef[0] != undefined && vehicleRef[0] != undefined) {
+                        console.log(stopPointRef[0].innerHTML + ' == ' +  busStopId);
 
-                            if (stopPointRef[0].innerHTML == busStopId) {
-                                isNextStop = true;
-                                vehicleRefNum = vehicleRef[0].innerHTML;
-                                return false;
-                            }
+                        if (stopPointRef[0].innerHTML == busStopId) {
+                            isNextStop = true;
+                            vehicleRefNum = vehicleRef[0].innerHTML;
+                            return false;
                         }
-                    });
-
-                    if (isNextStop) {
-                        clearInterval(refreshInterval);
-
-                        var payload = [busId, vehicleRefNum];
-                        payload = payload.toString();
-
-                        webPush.sendNotification(
-                            pushSubscriptions,
-                            payload,
-                            options
-                        );
-                        console.log('Send push notification::')
                     }
-                },
-                error: function (error) {
-                    console.log(error);
+                });
+
+                if (isNextStop) {
+                    clearInterval(refreshInterval);
+
+                    var payload = [busId, vehicleRefNum];
+                    payload = payload.toString();
+
+                    webPush.sendNotification(
+                        pushSubscriptions,
+                        payload,
+                        options
+                    );
+                    console.log('Send push notification::')
                 }
-            });
-        }, 10000);
-    });
+            },
+            error: function (error) {
+                console.log(error);
+            }
+        });
+    }, 10000);
 
     res.send('OK');
 });
